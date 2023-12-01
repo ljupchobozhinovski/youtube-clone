cebcc3c32c96b946c8f353afd1a0bc5c03994dbc
Ljupcho Bozhinovski <ljupchobozhinovski@gmail.com>
2023-12-01 14:12:44 +0100

    init project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import "./index.css";
+
+const App = () => <div>App</div>;
+
+export default App;
